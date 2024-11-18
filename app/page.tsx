d76ca806ec21e52e4899794d0f58f73a4cbd84aa
Ririn <caterin.ririn@gmail.com>
2024-11-18 09:55:35 +0700

    make tab profile and about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,44 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
+import Image from 'next/image'
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tab, setTab] = useState('profile');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="flex items-center flex-col">
+      <div className="flex flex-col max-w-[720px] w-full">
+      <div className="flex">
+        <button onClick={() => setTab('profile')} className="focus:outline-2 focus:outline-[goldenrod] outline outline-offset-4 p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center"
+        >Profile</button>
+        <button onClick={() => setTab('about-me')} className="focus:outline-2 focus:outline-[goldenrod] outline outline-offset-4 p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center">About Me</button>
+      </div>
+      <div>
+        {
+          tab === "profile" ? (
+            <div className="m-4">
+              <h1>Profile</h1>
+              <p>The page your profile page</p>
+              <div className="my-8">
+                <Image src="https://pbs.twimg.com/profile_images/1434881675255861248/Grx8VDTK_400x400.jpg" alt="Image Profile" width={240} height={240} className="outline outline-black outline-offset-2 outline-2 rounded-2xl"/>
+                <h2 className="mt-3">Uncle Roger</h2>
+              </div>
+            </div>
+          ) : (
+            <div className="m-4">
+              <h1>About Me</h1>
+              <p className="mt-2">This page is telling about your self</p>
+              <div className="my-8">
+                <h2>Uncle Roger</h2>
+                <p className="mt-3 leading-loose">Nigel Ng Kin-ju[a] (/ʌŋ/ UNG; born 1990/1991) is a Malaysian comedian and YouTuber. He is best known for co-creating (alongside Evelyn Mok) and portraying Uncle Roger, a character representing a middle-aged Asian uncle with an exaggerated Cantonese accent who is usually seen aggressively critiquing people's attempts at cooking Asian food.</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </div>
     </div>
   );
 }
