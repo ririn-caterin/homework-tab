609f36837d5ddc774aa05df4fdc6adb14c58299d
Ririn <caterin.ririn@gmail.com>
2024-11-22 08:56:34 +0700

    create form page

---
+++ app/form/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState, ChangeEvent } from "react";
+import { ChevronUp, ChevronDown } from "lucide-react";
+
+const Form = () => {
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [checkedCount, setCheckedCount] = useState<number>(0);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  const categories = [
+    "Complaint",
+    "Information Request",
+    "Questions",
+    "Others",
+  ];
+  const totalCategories = categories.length;
+
+  const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (e.target.checked) {
+      setSelectedCategories((prev) => [...prev, value]);
+      setCheckedCount((prevCount) => prevCount + 1);
+    } else {
+      setSelectedCategories((prev) =>
+        prev.filter((category) => category !== value)
+      );
+      setCheckedCount((prevCount) => prevCount - 1);
+    }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsSubmitted(true);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col w-full max-w-[360px]">
+        <div className="flex flex-col my-4">
+          <label>Email</label>
+          <input
+            type="text"
+            className="border p-3 border-gray-400 rounded-lg"
+            placeholder="Input email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div className="flex flex-col mb-4">
+          <label>Message Categories</label>
+          <div className="relative w-full">
+            <div
+              className="flex items-center justify-between border p-3 border-gray-400 rounded-lg"
+              onClick={toggleDropdown}
+            >
+              <div>
+                <span>Categories: </span>
+                <span>
+                  {checkedCount === totalCategories ? "All" : checkedCount}
+                </span>
+              </div>
+              {isDropdownOpen === true ? <ChevronUp /> : <ChevronDown />}
+            </div>
+            {isDropdownOpen && (
+              <div className="flex flex-col absolute w-full border border-b-0 z-10 border-gray-400 rounded-lg bg-white mt-3 overflow-hidden">
+                {categories.map((category, index) => (
+                  <label
+                    key={index}
+                    className="w-full block border-b-[1px] border-gray-400 p-3"
+                  >
+                    <input
+                      type="checkbox"
+                      value={category}
+                      onChange={handleCheckboxChange}
+                      checked={selectedCategories.includes(category)}
+                    />
+                    <span className="ml-2">{category}</span>
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col mb-4">
+          <label>Message</label>
+          <textarea
+            className="border p-3 border-gray-400 rounded-lg"
+            placeholder="Input your message..."
+            onChange={(e) => setMessage(e.target.value)}
+            rows={5}
+          ></textarea>
+        </div>
+        <div className="flex flex-col mb-4">
+          <button
+            className="bg-slate-900 text-white rounded-lg py-3 px-4"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      {isSubmitted && (
+        <div className="flex flex-col w-full max-w-[360px]">
+          <div>Email: {email}</div>
+          <div>
+            Selected Categories:
+            {selectedCategories.length > 0 &&
+              selectedCategories.map((category, index) => (
+                <p key={index}>{category}</p>
+              ))}
+          </div>
+          <div>Message: {message}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Form;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
 
 import { useState } from "react";
-import Image from "next/image"
-
+import Image from "next/image";
 
 export default function Home() {
   const [tab, setTab] = useState("profile");
@@ -10,23 +9,41 @@ export default function Home() {
   return (
     <div className="flex items-center flex-col">
       <div className="flex flex-col max-w-[720px] w-full">
-      <div className="flex">
-        <button onClick={() => setTab("profile")} className={`p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center ${
-              tab === "profile" ? "outline outline-2 outline-[goldenrod] outline-offset-4" : ""
+        <div className="flex">
+          <button
+            onClick={() => setTab("profile")}
+            className={`p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center ${
+              tab === "profile"
+                ? "outline outline-2 outline-[goldenrod] outline-offset-4"
+                : ""
             }`}
-        >Profile</button>
-        <button onClick={() => setTab("about-me")} className={`p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center ${
-              tab === "about-me" ? "outline outline-2 outline-[goldenrod] outline-offset-4" : ""
-            }`}>About Me</button>
-      </div>
-      <div>
-        {
-          tab === "profile" ? (
+          >
+            Profile
+          </button>
+          <button
+            onClick={() => setTab("about-me")}
+            className={`p-2 m-4 bg-neutral-900 text-white rounded-xl w-full text-center ${
+              tab === "about-me"
+                ? "outline outline-2 outline-[goldenrod] outline-offset-4"
+                : ""
+            }`}
+          >
+            About Me
+          </button>
+        </div>
+        <div>
+          {tab === "profile" ? (
             <div className="m-4">
               <h1>Profile</h1>
               <p>The page your profile page</p>
               <div className="my-8">
-                <Image src="https://pbs.twimg.com/profile_images/1434881675255861248/Grx8VDTK_400x400.jpg" alt="Image Profile" width={240} height={240} className="outline outline-black outline-offset-2 outline-2 rounded-2xl"/>
+                <Image
+                  src="https://pbs.twimg.com/profile_images/1434881675255861248/Grx8VDTK_400x400.jpg"
+                  alt="Image Profile"
+                  width={240}
+                  height={240}
+                  className="outline outline-black outline-offset-2 outline-2 rounded-2xl"
+                />
                 <h2 className="mt-3">Uncle Roger</h2>
               </div>
             </div>
@@ -36,13 +53,19 @@ export default function Home() {
               <p className="mt-2">This page is telling about your self</p>
               <div className="my-8">
                 <h2>Uncle Roger</h2>
-                <p className="mt-3 leading-loose">Nigel Ng Kin-ju[a] (/ʌŋ/ UNG; born 1990/1991) is a Malaysian comedian and YouTuber. He is best known for co-creating (alongside Evelyn Mok) and portraying Uncle Roger, a character representing a middle-aged Asian uncle with an exaggerated Cantonese accent who is usually seen aggressively critiquing peoples attempts at cooking Asian food.</p>
+                <p className="mt-3 leading-loose">
+                  Nigel Ng Kin-ju[a] (/ʌŋ/ UNG; born 1990/1991) is a Malaysian
+                  comedian and YouTuber. He is best known for co-creating
+                  (alongside Evelyn Mok) and portraying Uncle Roger, a character
+                  representing a middle-aged Asian uncle with an exaggerated
+                  Cantonese accent who is usually seen aggressively critiquing
+                  peoples attempts at cooking Asian food.
+                </p>
               </div>
             </div>
-          )
-        }
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
